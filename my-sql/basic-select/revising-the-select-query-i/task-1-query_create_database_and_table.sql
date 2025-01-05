223ea54b134ad270dcfd3c2fe094ee924593db89
nezno0 <zenitechq@gmail.com>
2025-01-05 01:15:44 +0100

    feat(basic-select/revising-the-select-query-i): solved task revising_the_select_query_i

---
+++ my-sql/basic-select/revising-the-select-query-i/task-1-query_create_database_and_table.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS `mysql_database`;
+
+USE `mysql_database`;
+
+CREATE TABLE IF NOT EXISTS `city` (
+    `id` INT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(17),
+    `country_code` VARCHAR(3),
+    `district` VARCHAR(20),
+    `population` INT
+);
+
+INSERT IGNORE INTO `city` (`id`, `name`, `country_code`, `district`, `population`) VALUES
+(6, 'Rotterdam', 'NLD', 'Zuid-Holland', 593321),
+(3878, 'Scottsdale', 'USA', 'Arizona', 202705),
+(3965, 'Corona', 'USA', 'California', 124966),
+(3973, 'Concord', 'USA', 'California', 121780),
+(3977, 'Cedar Rapids', 'USA', 'Iowa', 120758),
+(3982, 'Coral Springs', 'USA', 'Florida', 117549),
+(4054, 'Fairfield', 'USA', 'California', 92256),
+(4058, 'Boulder', 'USA', 'Colorado', 91238),
+(4061, 'Fall River', 'USA', 'Massachusetts', 90555);
+
+SELECT * FROM `city` WHERE `country_code` = 'USA' AND `population` > 100000;
