cb6da827cce78bda3708352bcd91dfa5d4d5b0b0
nezno0 <zenitechq@gmail.com>
2025-01-06 16:39:33 +0100

    feat(basic-select/japanese-cities-names): solved task japanese-cities-names

---
+++ my-sql/basic-select/japanese-cities-names/task-5-hr-query-create-database-and-table.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS `hr_database`;
+
+USE `hr_database`;
+
+CREATE TABLE IF NOT EXISTS `CITY` (
+    `ID` INT NOT NULL PRIMARY KEY,
+    `NAME` VARCHAR(17),
+    `COUNTRYCODE` VARCHAR(3),
+    `DISTRICT` VARCHAR(20),
+    `POPULATION` INT
+);
+
+INSERT INTO `CITY` (`ID`, `NAME`, `COUNTRYCODE`, `DISTRICT`, `POPULATION`) VALUES
+(6, 'Rotterdam', 'NLD', 'Zuid-Holland', 593321),
+(3878, 'Scottsdale', 'USA', 'Arizona', 202705),
+(3965, 'Corona', 'USA', 'California', 124966),
+(3973, 'Concord', 'USA', 'California', 121780),
+(3977, 'Cedar Rapids', 'USA', 'Iowa', 120758),
+(3982, 'Coral Springs', 'USA', 'Florida', 117549),
+(4054, 'Fairfield', 'USA', 'California', 92256),
+(4058, 'Boulder', 'USA', 'Colorado', 91238),
+(4061, 'Fall River', 'USA', 'Massachusetts', 90555)
+AS `CITYNEW`
+ON DUPLICATE KEY UPDATE 
+    `NAME` = `CITYNEW`.`NAME`,
+    `COUNTRYCODE` = `CITYNEW`.`COUNTRYCODE`,
+    `DISTRICT` = `CITYNEW`.`DISTRICT`,
+    `POPULATION` = `CITYNEW`.`POPULATION`;
+
+SELECT `NAME` FROM `CITY` WHERE `COUNTRYCODE` = 'JPN';
---
+++ my-sql/basic-select/japanese-cities-names/task-5-query_create_database_and_table.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS `mysql_database`;
+
+USE `mysql_database`;
+
+CREATE TABLE IF NOT EXISTS `city` (
+    `id` INT NOT NULL PRIMARY KEY,
+    `name` VARCHAR(17),
+    `country_code` VARCHAR(3),
+    `district` VARCHAR(20),
+    `population` INT
+);
+
+INSERT INTO `city` (`id`, `name`, `country_code`, `district`, `population`) VALUES
+(6, 'Rotterdam', 'NLD', 'Zuid-Holland', 593321),
+(3878, 'Scottsdale', 'USA', 'Arizona', 202705),
+(3965, 'Corona', 'USA', 'California', 124966),
+(3973, 'Concord', 'USA', 'California', 121780),
+(3977, 'Cedar Rapids', 'USA', 'Iowa', 120758),
+(3982, 'Coral Springs', 'USA', 'Florida', 117549),
+(4054, 'Fairfield', 'USA', 'California', 92256),
+(4058, 'Boulder', 'USA', 'Colorado', 91238),
+(4061, 'Fall River', 'USA', 'Massachusetts', 90555)
+ON DUPLICATE KEY UPDATE 
+    `name` = VALUES(`name`),
+    `country_code` = VALUES(`country_code`),
+    `district` = VALUES(`district`),
+    `population` = VALUES(`population`);
+
+SELECT `name` FROM `city` WHERE `country_code` = 'JPN';
